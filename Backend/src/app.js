f6536944cfe89ba9b729cf24a1629c55fd9df13c
solvingop <sachitk919@gmail.com>
2026-05-03 19:07:30 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://ai-resume-analyzer-eight-taupe.vercel.app/login",
     credentials: true
 }))
 
